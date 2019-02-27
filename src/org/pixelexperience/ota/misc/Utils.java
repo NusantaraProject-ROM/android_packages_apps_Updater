78351bc5a86f9d2e75a790d01b3949c11aba32ec
shagbag913 <sh4gbag913@gmail.com>
2019-02-27 10:21:42 -0500

    Updates: update changelog dialog

--- src/org/pixelexperience/ota/misc/Utils.java
+++ src/org/pixelexperience/ota/misc/Utils.java
@@ -34,6 +34,7 @@ import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import org.pixelexperience.ota.UpdatesDbHelper;
 import org.pixelexperience.ota.controller.UpdaterService;
 import org.pixelexperience.ota.model.Update;
@@ -151,6 +152,12 @@ public class Utils {
 
     public static String getDownloadWebpageUrl(String fileName) {
         return String.format(Constants.DOWNLOAD_WEBPAGE_URL, SystemProperties.get(Constants.PROP_DEVICE), fileName);
+    }
+
+    public static String getChangelogURL(Context context) {
+        String device = SystemProperties.get(Constants.PROP_VERSION_CODE,
+                SystemProperties.get(Constants.PROP_DEVICE));
+        return context.getString(R.string.changelog_url, device);
     }
 
     public static void triggerUpdate(Context context, String downloadId) {
